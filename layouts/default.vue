eab83f07aef904432669f87770c2992e873253ad
DESKTOP-9QRQHUG\rongm <rongmarin98@gmail.com>
2023-04-08 12:06:40 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,91 +1,40 @@
 <template>
     <div>
       <!-- Start Header -->
-      
-        <b-container fluid 
-          :class="$colorMode.value=='dark'?'bg-dark' : 'bg-white' " 
-          class="position-fixed header"
-          style="border-bottom:0.1px solid #ededed">
-            <b-navbar toggleable="lg">
-              <b-container class="p-2">
-                <b-navbar-brand to="/">
-                  <img height="50" :src="`${api_key}/image_etec/${header.image}`" alt="">
+        <header id="navigation-bar" class="p-1">
+          <b-container>
+            <header>
+              <b-navbar toggleable="lg">
+                <b-navbar-brand href="#">
+                  <img height="35" src="https://dreamslms-wp.dreamguystech.com/wp-content/themes/dreamslms/assets/images/logo.svg" alt="">
                 </b-navbar-brand>
-    
-                <b-navbar-toggle 
-                  target="nav-collapse" 
-                  class="ml-auto"
-                >
-                <span class="material-icons" :class="$colorMode.value=='dark'?'text-dark-mode' : 'text-light-mode'">menu</span>
-              </b-navbar-toggle>
-    
+            
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            
                 <b-collapse id="nav-collapse" is-nav>
-                  <b-navbar-nav class="m-auto" :class="$colorMode.value=='dark'?'text-dark-mode' : 'text-light-mode'">
-                    <b-nav-item class="pl-4" to="/" >
-                      {{ $t('home') }}
-                    </b-nav-item>
-                    <b-nav-item class="pl-4" to="/Article">
-                      {{ $t('article') }}
-                    </b-nav-item>
-                    <b-nav-item class="pl-4" to="/Course">
-                      {{ $t('course') }}
-                    </b-nav-item>
-                    <b-nav-item class="pl-4" to="/Video">
-                      {{ $t('video') }}
-                    </b-nav-item>
-                    <b-nav-item class="pl-4" to="/Job">
-                      {{ $t('job') }}
-                    </b-nav-item>
-                  </b-navbar-nav>
+           
+                    <b-navbar-nav class="m-auto">
+                      <b-nav-item active class="pl-4" to="/">Home</b-nav-item>
+                      <b-nav-item class="pl-4" href="#">About Us</b-nav-item>
+                      <b-nav-item class="pl-4" href="#">Instructors</b-nav-item>
+                      <b-nav-item class="pl-4" href="#">Students</b-nav-item>
+                      <b-nav-item class="pl-4" href="#">Contact Us</b-nav-item>
+                    </b-navbar-nav>
+                 
+                  
+            
                   <!-- Right aligned nav items -->
-                  <b-navbar-nav class="m-right">
+                  <b-navbar-nav class="ml-auto">
                     <b-nav-form>
-                      <b-form-input v-model="search" size="sm" class="mr-sm-2" :placeholder="$t('search')+'...'"></b-form-input>
-                      <b-button size="sm" 
-                                class="my-2 my-sm-0 orange-bg" 
-                                type="button"
-                                @click.prevent.stop="onSearch"
-                      >{{ $t('search') }}</b-button>
+                      <b-button pill variant="light" class="mr-2 login">Login</b-button>
+                      <b-button pill class="register">Register</b-button>
                     </b-nav-form>
-
-                    <client-only>
-                      <b-nav-item-dropdown class="ml-2 mr-1" right>
-                        <template #text>
-                          <span v-if="$i18n.locale == 'en'" style="padding:0px">
-                            <img style="padding:0px" height="18" width="20" src="@/assets/image/en.png" alt="">
-                          </span>
-                          <span v-else>
-                            <img style="padding:0px" height="18" width="20" src="@/assets/image/kh.png" alt="">
-                          </span>
-                        </template>
-                        <b-dropdown-item @click.prevent.stop="$i18n.setLocale('en')" >
-                          <img style="padding:0px" height="18" width="20" src="@/assets/image/en.png" alt=""> {{ $t('english') }}
-                        </b-dropdown-item>
-                        <b-dropdown-item @click.prevent.stop="$i18n.setLocale('kh')">
-                          <img style="padding:0px" height="18" width="20" src="@/assets/image/kh.png" alt=""> {{ $t('khmer') }}
-                        </b-dropdown-item>
-                      </b-nav-item-dropdown>
-                    </client-only>
-
-                    <b-nav-item>
-                      <span v-if="$colorMode.value=='dark'"
-                        @click="$colorMode.value='light'"
-                        class="material-icons"
-                        :class="$colorMode.value=='dark'?'text-dark-mode' : 'text-light-mode'"
-                      >brightness_4</span>
-                      <span v-if="$colorMode.value!='dark'"
-                        @click="$colorMode.value='dark'"
-                        class="material-icons"
-                        :class="$colorMode.value=='dark'?'text-dark-mode' : 'text-light-mode'"
-                      >nights_stay</span>
-                    </b-nav-item>
                   </b-navbar-nav>
-    
-                </b-collapse> 
-            </b-container>
-            </b-navbar>
-        </b-container>
-      
+                </b-collapse>
+              </b-navbar>
+            </header>
+          </b-container>
+        </header>
       <!-- End Header -->
 
       <Nuxt />
@@ -138,49 +87,26 @@ export default{
     data(){
         return {
           api_key: process.env.BASE_URL,
-          address: '',
-          about: '',
-          footer: '',
-          header: '',
-          social: '',
-          search: ''
         }
     },
     mounted(){
-      this.getHeader()
+        let nav = document.getElementById("navigation-bar");
+        let login_button = document.querySelector('.login')
+        let sticky = nav.offsetTop;
+        window.onscroll = function() {sticker()};
+        function sticker() {
+          if (window.pageYOffset >= sticky) {
+              nav.style.top="0px"
+              login_button.classList.add("login-custome")
+              nav.classList.add("sticky")
+          } else {
+              nav.style.top="10px"
+              login_button.classList.remove("login-custome");
+              nav.classList.remove("sticky");
+          }
+      }
     },
     methods: {
-       category(type){
-          this.$router.push('Category?type='+type+'')
-        },
-        getAddress(){
-          this.$axios.$get('getAddress').then(response => {
-            this.address = response
-          })
-        },
-        getAbout(){
-          this.$axios.$get('getAbout').then(response => {
-            this.about = response
-          })
-        },
-        getFooter(){
-          this.$axios.$get('getAllLogoFooter').then(response => {
-            this.footer = response
-          })
-        },
-        getHeader(){
-          this.$axios.$get('getAllLogoHeader').then(response => {
-            this.header = response
-          })
-        },
-        getSocial(){
-          this.$axios.$get('getAllSocialMedia').then(response => {
-            this.social = response.data
-          })
-        },
-        getImage(image) {
-          return 'https://etec-api.loveounnas.xyz/image_etec/' + image
-        },
         switchLang(locale){
           if (this.$i18n.locale !== locale) {
               this.$i18n.locale = locale;
@@ -191,7 +117,6 @@ export default{
           this.$router.push({path: `/Search/${this.search}`});
         }
     }
-   
 }
 </script>
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,57 @@
 <template>
   <div>
+    <main>
+      <b-container class="h-100">
+        <b-row class="h-100">
+          <b-col cols="6" class="main-left">
+            <h4>The Leader in Online Learning</h4>
+            <h1>
+              <b class="color-dark-blue">Engaging & <br> Accessible Online <br> Courses For All</b>
+            </h1>
+            <div class="search mt-4">
+              <span class="icon">
+                <i class="material-icons">search</i>
+              </span>
+              <input type="text" placeholder="Search School, Online education center, etc">
+              <span class="arrow">
+                <i class="material-icons">arrow_forward</i>
+              </span>
+            </div>
+            <h4 class="mt-4">Trusted by over 15K Users <br>
+              worldwide since 2022</h4>
+            <div class="d-flex align-items-center" style="margin-top:-1.5rem">
+               <h1><b class="color-dark-blue">290+</b></h1>
+               <h4 class="color-dark-blue ml-2">Courses</h4>
+            </div>
+           
 
-      
-    
+          </b-col>
+          <b-col cols="6" class="main-right">
+            <img height="500" src="https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/object.png" alt="">
+          </b-col>
+        </b-row>
+      </b-container>
+    </main>
+
+    <!-- start course category -->
+    <section class="top-category">
+      <b-container class="position-relative">
+        <div class="category">
+          <div class="box-category"></div>
+          <div class="box-category"></div>
+          <div class="box-category"></div>
+          <div class="box-category"></div>
+        </div>
+
+        <b-row class="category-title">
+          <b-col cols="6">
+            <h5><b class="color-red">Favourite Course</b></h5>
+            <h2><b class="color-dark-blue">Top Category</b></h2>
+          </b-col>
+          <b-col cols="6"></b-col>
+        </b-row>
+      </b-container>
+    </section>
   </div>
 </template>
 
