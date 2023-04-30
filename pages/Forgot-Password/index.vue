44a31d778094ff77053f5b4952a3a1c871fbb541
rongmarin <mrlucky5868@gmail.com>
2023-04-30 16:54:30 +0700

    update

--- pages/Forgot-Password/index.vue
+++ pages/Forgot-Password/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="forgot-password-box">
     <b-card class="text-center">
+      <form action="">
       <i
         class="material-icons"
         style="font-size: 75px; color: var(--color-red)"
@@ -13,22 +14,35 @@
           $t("enter_your_email_and_we_ll_send_a_link_to_reset_your_password")
         }}
       </p>
+
       <div class="form">
-        <div class="email-input d-flex align-items-center">
-          <i class="material-icons" style="font-size: 48px">mail</i>
-          <b-input placeholder="Email" type="email"> </b-input>
+        <div class="email-input d-flex align-items-center form-border">
+          <i class="material-icons pl-2" >mail</i>
+          
+            <b-input 
+              placeholder="Email" 
+              type="email"
+              v-model="form.email"
+              required
+            > </b-input>
+          
         </div>
       </div>
 
-      <b-alert show variant="danger" class="text-left mt-2">
-        email not found
-      </b-alert>
-      <b-button variant="primary" size="lg" class="mt-1 w-100">{{
+      <p v-if="response && status" class="text-success text-left mt-3 mb-0">
+        {{ message }}
+      </p>
+      <p v-else-if="response && status==false" class="text-danger text-left mt-3 mb-0">
+        {{ message }}
+      </p>
+
+      <b-button @click="requestResetPassword()" variant="success" size="lg" class="mt-3 w-100">{{
         $t("submit")
       }}</b-button>
       <nuxt-link to="Login" class="d-block m-3">
         {{ $t("back_to_login") }}
       </nuxt-link>
+     </form>
     </b-card>
   </div>
 </template>
@@ -36,6 +50,25 @@
 <script>
 export default {
   layout: "noLayout",
+  data(){
+    return {
+      form: {
+        email: ''
+      },
+      response: false,
+      message: '',
+      status: false
+    }
+  },
+  methods: {
+    requestResetPassword(){
+      this.$axios.$post('requestResetPassword',this.form).then(res => {
+        this.status = res.status
+        this.response = true
+        this.message = res.message
+      })
+    }
+  }
 };
 </script>
 
